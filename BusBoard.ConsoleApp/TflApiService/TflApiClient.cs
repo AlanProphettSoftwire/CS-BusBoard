f84aa5baf2270ec3e70eb237abb525d3c5c7cfc7
AlanProphettSoftwire <alan.prophett@softwire.com>
2025-09-18 11:18:55 +0100

    add GetApiResponse search param options via polymorthism and refactor logic to load in user secrets

--- BusBoard.ConsoleApp/TflApiService/TflApiClient.cs
+++ BusBoard.ConsoleApp/TflApiService/TflApiClient.cs
@@ -8,26 +8,42 @@ public class TflApiClient
 {
     private readonly RestClient _client;
     
-    public TflApiClient(IConfiguration secretConfiguration)
+    public TflApiClient()
     {
+        var config = new ConfigurationBuilder()
+            .AddUserSecrets<Program>()
+            .AddEnvironmentVariables()
+            .Build();
+        
         RestClientOptions options = new RestClientOptions("https://api.tfl.gov.uk/");
         _client = new RestClient(
             options,
             configureSerialization: s => s.UseSystemTextJson());
 
-        string tflSecretKey = secretConfiguration["tflApi:key"];
+        string tflSecretKey = config["tflApi:key"];
         if (string.IsNullOrWhiteSpace(tflSecretKey))
         {
             throw new Exception("Tfl API key not configured");
         }
-            
         
-        _client.AddDefaultParameter("app_key",secretConfiguration["tflApi:key"]);
+        _client.AddDefaultParameter("app_key", tflSecretKey);
     }
-    
+
     public T GetApiResponse<T>(string resource)
     {
+        return this.GetApiResponse<T>(resource, []);
+    }
+
+    
+    public T GetApiResponse<T>(string resource, List<Tuple<string, string>> queryOptions)
+    {
         var request = new RestRequest(resource);
+        
+        foreach (var queryOption in queryOptions)
+        {
+            request.AddParameter(queryOption.Item1, queryOption.Item2);
+        }
+        
         var response = _client.Execute<T>(request);
 
         if (response.StatusCode != System.Net.HttpStatusCode.OK)
