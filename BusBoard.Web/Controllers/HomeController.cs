7cbb7b8958e643cb9846c14822986051cb257ee8
AlanProphettSoftwire <alan.prophett@softwire.com>
2025-09-19 17:11:31 +0100

    add features on webapp

--- BusBoard.Web/Controllers/HomeController.cs
+++ BusBoard.Web/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 using System.Runtime.InteropServices.JavaScript;
+using BusBoard.Api.PostcodeApiService;
 using BusBoard.Api.TflModels;
 using Microsoft.AspNetCore.Mvc;
 using BusBoard.Web.Models;
@@ -11,7 +12,47 @@ public class HomeController : Controller
 {
     public IActionResult Index(PostcodeSelection selection)
     {
-        return View();
+        PostcodeApiClient postcodeApiClient = new PostcodeApiClient();
+
+        if (string.IsNullOrWhiteSpace(selection.Postcode))
+        {
+            var mapDetails = new MapDetails(selection.Postcode, null, null, null, null);
+            return View(mapDetails);
+        }
+
+        try
+        {
+            PostcodeResponseModel postcodeLongLat = postcodeApiClient.GetLonLatFromPostcode(selection.Postcode);
+            Actions tflActions = new Actions();
+            try
+            {
+                List<StopPoint> stopPoints = tflActions.GetStopsInRadiusOfLocation(postcodeLongLat.Result.Latitude,
+                    postcodeLongLat.Result.Longitude, radius: 600);
+
+                if (stopPoints.Count > 0)
+                {
+                    var mapDetails = new MapDetails(selection.Postcode, postcodeLongLat.Result.Latitude,
+                        postcodeLongLat.Result.Longitude, stopPoints, null);
+                    return View(mapDetails);
+                }
+                else
+                {
+                    var mapDetails = new MapDetails(selection.Postcode, postcodeLongLat.Result.Latitude,
+                        postcodeLongLat.Result.Longitude, stopPoints, "☹️ No stops nearby");
+                    return View(mapDetails);
+                }
+            }
+            catch (Exception ex)
+            {
+                return View("Error");
+            }
+
+        }
+        catch (Exception ex)
+        {
+            var mapDetails = new MapDetails(selection.Postcode, null, null, null, $"🗺️ Your postcode '{selection.Postcode}' does not exist.");
+            return View(mapDetails);
+        }
     }
 
     [HttpGet]
---
+++ BusBoard.Web/ViewModels/MapDetails.cs
@@ -0,0 +1,25 @@
+﻿namespace BusBoard.Web.ViewModels;
+
+public class MapDetails
+{
+    private static readonly double LondonLongitude = 0.1276;
+    private static readonly  double LondonLatitude = 51.5072;
+    
+    public string PostCode { get; set; }
+    public double Longitude { get; set; }
+    public double Latitude { get; set; }
+    
+    public string Message { get; set; }
+    
+    public List<StopPoint> NearbyStopPoints { get; set; }
+    
+    public MapDetails(string? postCode, double? latitude, double? longitude,  List<StopPoint>? nearbyStopPoints, string? message)
+    {
+        Message = message ?? "";
+        PostCode = postCode ?? "";
+        Longitude = longitude ?? LondonLongitude;
+        Latitude = latitude ?? LondonLatitude;
+        NearbyStopPoints = nearbyStopPoints ?? [];
+    }
+
+}
